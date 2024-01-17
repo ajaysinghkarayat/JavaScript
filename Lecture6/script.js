5f302172d7a198991c40d64189df12709afe5980
ajaysinghkarayat <ajaysinghkarayat2@gmail.com>
2024-01-18 04:37:58 +0530

    add

--- Lecture6/script.js
+++ Lecture6/script.js
@@ -3,3 +3,12 @@ console.dir(heading)
 
 let headingclass = document.getElementsByClassName("heading-class")
 console.dir(headingclass)
+
+let para = document.getElementsByTagName("p")
+console.dir(para)
+
+let firstel = document.querySelector("p");
+console.dir(firstel);
+
+let allel = document.querySelectorAll("p");
+console.dir(allel);
