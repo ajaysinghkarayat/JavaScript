10bb9c145f81ffe270c5d443fdb45f9d5564e972
ajaysinghkarayat <ajaysinghkarayat2@gmail.com>
2024-01-19 00:14:49 +0530

    add

--- Lecture6/script.js
+++ Lecture6/script.js
@@ -14,3 +14,7 @@ let allel = document.querySelectorAll("p");
 console.dir(allel);
 
 let head1 = document.querySelector("h1");
+
+let h2 = document.getElementById("add")
+console.dir(h2)
+h2.append(" from Apna College students")
