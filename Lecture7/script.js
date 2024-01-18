d6a2d5a48ce4c4a2a4a179c789962ef778c74402
ajaysinghkarayat <ajaysinghkarayat2@gmail.com>
2024-01-19 03:03:18 +0530

    add

---
+++ Lecture7/script.js
@@ -0,0 +1,11 @@
+let div = document.querySelector("div");
+console.dir(div);
+
+let val = div.getAttribute("id");
+console.log(val);
+
+console.log(div.setAttribute("id", "newbox"));
+
+div.style.backgroundColor = "blue";
+div.style.fontSize = "34px";
+
