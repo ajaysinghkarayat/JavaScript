3d1b0b7c016e866decd93bc3b6f2a517eb6504c5
ajaysinghkarayat <ajaysinghkarayat2@gmail.com>
2024-01-19 02:05:49 +0530

    add

--- Lecture6/script.js
+++ Lecture6/script.js
@@ -1,20 +1,34 @@
-let heading = document.getElementById("heading")
-console.dir(heading)
+// let heading = document.getElementById("heading")
+// console.dir(heading)
 
-let headingclass = document.getElementsByClassName("heading-class")
-console.dir(headingclass)
+// let headingclass = document.getElementsByClassName("heading-class")
+// console.dir(headingclass)
 
-let para = document.getElementsByTagName("p")
-console.dir(para)
+// let para = document.getElementsByTagName("p")
+// console.dir(para)
 
-let firstel = document.querySelector("p");
-console.dir(firstel);
+// let firstel = document.querySelector("p");
+// console.dir(firstel);
 
-let allel = document.querySelectorAll("p");
-console.dir(allel);
+// let allel = document.querySelectorAll("p");
+// console.dir(allel);
 
-let head1 = document.querySelector("h1");
+// let head1 = document.querySelector("h1");
 
-let h2 = document.getElementById("add")
-console.dir(h2)
-h2.append(" from Apna College students")
+// let h2 = document.getElementById("add")
+// console.dir(h2)
+// h2.append(" from Apna College students")
+
+//....................
+let divall = document.querySelectorAll(".box");
+
+console.log(divall[0]);
+console.log(divall[1]);
+//..............
+
+let idx = 1;
+for(div of divall)
+{
+    div.innerText = `new unique value ${idx}`;
+    idx++;
+}
