0902be892e85165c20c2e69ba42d895903431b45
ajaysinghkarayat <ajaysinghkarayat2@gmail.com>
2024-01-26 23:34:50 +0530

    add

---
+++ Lecture8/script.js
@@ -0,0 +1,21 @@
+let button = document.querySelector("button");
+
+// button.onclick = () => {
+//     console.log("Button is clicked");
+// };
+
+// button.onclick = (evt) => {
+//     console.log(evt);
+// };
+
+// button.onmousedown = () => {
+//     console.log("Button is pressed");
+// };
+
+// button.onmousemove = () => {
+//     console.log("Button is pressed");
+// };
+
+button.onmouseenter = () => {
+    console.log("Button is pressed");
+};
